041837ecb23db5faf28f7e28d36afaa2006e1075
klegul <klegul@pm.me>
2024-06-06 11:39:50 +0200

    Bump version to 0.3.3

--- main/src/main/java/com/shirkanesi/magmaplayer/util/MagmaPlayerInfo.java
+++ main/src/main/java/com/shirkanesi/magmaplayer/util/MagmaPlayerInfo.java
@@ -2,7 +2,7 @@ package com.shirkanesi.magmaplayer.util;
 
 public class MagmaPlayerInfo {
 
-    public static final String VERSION = "0.3.2";
+    public static final String VERSION = "0.3.3";
 
     private MagmaPlayerInfo() {}
 
