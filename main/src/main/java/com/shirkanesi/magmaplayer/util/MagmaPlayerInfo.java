93b8131b0aa068b9b3aa79c1ce1c7cf42c7fa097
klegul <klegul@pm.me>
2024-10-12 16:58:00 +0200

    Release 0.5.0

--- main/src/main/java/com/shirkanesi/magmaplayer/util/MagmaPlayerInfo.java
+++ main/src/main/java/com/shirkanesi/magmaplayer/util/MagmaPlayerInfo.java
@@ -2,7 +2,7 @@ package com.shirkanesi.magmaplayer.util;
 
 public class MagmaPlayerInfo {
 
-    public static final String VERSION = "0.4.0";
+    public static final String VERSION = "0.5.0";
 
     private MagmaPlayerInfo() {}
 
